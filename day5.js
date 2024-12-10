83a56d7a31866dc2988f0c57d3196a9680245e0f
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2024-12-09 18:41:53 -0600

    Day 5 complete

--- day5.js
+++ day5.js
@@ -4,6 +4,7 @@ const fs = require("node:fs/promises");
   let rules = [];
   let pages = [];
   let correct_pages = [];
+  let incorrect_pages = [];
 
   const rules_data = await fs.readFile("day5_rules.txt", { encoding: "utf-8" });
   rules = rules_data.split("\n");
@@ -27,13 +28,36 @@ const fs = require("node:fs/promises");
         }
       }
     });
-    if (rule_correct) correct_pages.push(page);
+
+    if (rule_correct) {
+      correct_pages.push(page);
+    } else {
+      incorrect_pages.push(page);
+    }
   });
+
+  for (let index = 0; index < 5; index++) {
+    incorrect_pages.forEach((page) => {
+      rules.forEach((rule) => {
+        if (page.includes(rule[0]) && page.includes(rule[1])) {
+          if (
+            page.findIndex((page) => page === rule[0]) >
+            page.findIndex((page) => page === rule[1])
+          ) {
+            page[page.indexOf(rule[0])] = page.splice(
+              page.indexOf(rule[1]),
+              1,
+              page[page.indexOf(rule[0])],
+            )[0];
+          }
+        }
+      });
+    });
+  }
 
   let answer = 0;
-  correct_pages.forEach((page) => {
+  incorrect_pages.forEach((page) => {
     answer += page[Math.floor(page.length / 2)];
   });
-
   console.log(answer);
 })();
